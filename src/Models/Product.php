a3ba187ea60e72e1f94a4b4db2ed45de071b824e
Kayraba Tourey <k.tourey@aston.ac.uk>
2025-10-20 11:12:47 +0100

    Fix product model logic

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -1,3 +1,11 @@
 <?php
-// Product model for CRUD operations
+class Myclass{
+    public function showTeam() {
+        $x = "Team 4";
+var_dump($x);
+echo "<br>";
+    }
+}
+$obj = new Myclass();
+$obj->showTeam();
 ?>

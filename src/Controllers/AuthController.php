e3fae162161a62f54931e2609bfff8e213c6d868
austin9794 <mohdumair72914@gmail.com>
2025-11-10 10:49:40 +0000

    Function to test routing

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -1,3 +1,9 @@
 <?php
-// Handles login, signup, and logout
+function logoutUser() {
+    // Temporary placeholder — to be replaced by Ayaan’s logic later
+    session_unset();
+    session_destroy();
+    header("Location: index.php?page=login");
+    exit;
+}
 ?>

9e5fab0bb957bde361b0feb009bef4db5af72cde
austin9794 <mohdumair72914@gmail.com>
2025-11-23 17:05:40 +0000

    database

--- src/Database.php
+++ src/Database.php
@@ -1,3 +1,24 @@
 <?php
-// Database connection using PDO
-?>
+class Database {
+    private static $instance = null;
+    private $conn;
+
+    private function __construct() {
+        $this->conn = new PDO(
+            "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8mb4",
+            DB_USER,
+            DB_PASS,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]
+        );
+    }
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+        return self::$instance->conn;
+    }
+}

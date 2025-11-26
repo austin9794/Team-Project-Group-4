ac70144e6f0f38c9a2413f352a40899f2d04b218
Ikram Ali <IkramAli127@hotmail.com>
2025-11-26 23:28:06 +0000

    set up basic checkout page

--- templates/customer/checkout.php
+++ templates/customer/checkout.php
@@ -1,1 +1,11 @@
-<!-- Checkout page -->
+<?php include __DIR__ . '/../header.php'; ?>
+
+<?php //simple checkout form  ?>
+
+<h2>checkout</h2>
+
+<form  method="post" action="/order/place">
+    
+</form>
+
+<?php include __DIR__ . '/../footer.php'; ?>

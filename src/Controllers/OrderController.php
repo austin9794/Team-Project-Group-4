017d85dafb229f46d4807bee1b519b76b16ca3ac
Ikram Ali <IkramAli127@hotmail.com>
2025-11-26 23:28:06 +0000

    add simple order controller to clear basket

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -1,3 +1,15 @@
 <?php
-// Handles order saving and history
-?>
+
+class OrderController
+{
+    //place order nd clear basket
+    public function place()
+    {
+        session_start() ;
+
+        //clear basket after order
+         unset($_SESSION['basket']);
+
+        echo 'order placed successfully';
+     }
+}

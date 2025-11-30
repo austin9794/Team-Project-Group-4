0fa3a5f4aeed6290f474405cc76e600365f50136
austin9794 <mohdumair72914@gmail.com>
2025-11-30 14:03:28 +0000

    guarantees session helpers are available

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,11 @@
+<?php 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once __DIR__ . '/../src/Helpers/session.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>

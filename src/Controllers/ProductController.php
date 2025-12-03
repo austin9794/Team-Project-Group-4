4181980e4faec4b309c9863cad5be737ac40a320
austin9794 <mohdumair72914@gmail.com>
2025-12-03 15:12:03 +0000

    fixing the constructor

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -1,7 +1,14 @@
 <?php
-require_once __DIR__ . "/../Models/Product.php";
-require_once __DIR__ . "/../Models/Database.php";
+require_once __DIR__ . '/../Database.php';
+
 class ProductController {
+
+    private $db;
+
+    public function __construct() {
+        $this->db = Database::getInstance();
+    }
+    
     public function listProducts($filters = []) {
         return Product::getAll($filters);
     }

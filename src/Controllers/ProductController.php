401a07b4d2e3401aa706ea13f1850090df35cb47
austin9794 <mohdumair72914@gmail.com>
2025-12-03 15:16:26 +0000

    get products by category

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -66,5 +66,28 @@ class ProductController {
         include __DIR__ . '/../../templates/customer/products.php';
     }
 
+    // GET PRODUCTS BY CATEGORY
+    
+    public function getByCategory() {
+
+        if (!isset($_GET['category'])) {
+            header("Location: /Team-Project-Group-4/public/index.php?page=products");
+            exit;
+        }
+
+        $catId = $_GET['category'];
+
+        $stmt = $this->db->prepare("
+            SELECT * FROM products 
+            WHERE category_id = ?
+        ");
+
+        $stmt->execute([$catId]);
+
+        $products = $stmt->fetchAll();
+
+        include __DIR__ . '/../../templates/customer/products.php';
+    }
+
 }
 ?>

872b3a0039f3673967dffc1f508a61a1aeb62f4a
austin9794 <mohdumair72914@gmail.com>
2025-12-03 15:23:08 +0000

    product detail page

--- public/index.php
+++ public/index.php
@@ -74,6 +74,10 @@ switch ($page) {
     case 'orders':
         include __DIR__ . '/../templates/customer/orders.php';
         break;
+        case 'product':
+        $controller = new ProductController();
+        $controller->getProductById();
+        break;
     case 'products':
         $controller = new ProductController();
     

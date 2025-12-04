db96007ee264201424e083608abfc8f54cd3fdc5
austin9794 <mohdumair72914@gmail.com>
2025-12-04 17:29:06 +0000

    login page

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -12,4 +12,9 @@ class AuthController {
         $this->db = Database::getInstance()->getConnection();
     }
 
+    // Show login page
+    public function showLogin() {
+        include __DIR__ . '/../../templates/auth/login.php';
+    }
+
     

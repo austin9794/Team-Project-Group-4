751a2691329d4bcfe3f817bf7d79b85bbbdf6444
austin9794 <mohdumair72914@gmail.com>
2025-12-04 17:45:28 +0000

    insert user

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -88,5 +88,17 @@ class AuthController {
     // 4. Hash the password properly
     $hashed = password_hash($password, PASSWORD_BCRYPT);
 
-    
+    // 5. Insert user
+    $stmt = $this->db->prepare("
+        INSERT INTO users (name, email, password, role, phone, address)
+        VALUES (?, ?, ?, 'customer', ?, ?)
+    ");
+
+    if ($stmt->execute([$name, $email, $hashed, $phone, $address])) {
+        header("Location: index.php?page=login&success=Account+created,+please+login");
+        exit;
+    }
+
+    header("Location: index.php?page=signup&error=An+error+occurred");
+}
 }

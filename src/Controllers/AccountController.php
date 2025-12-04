97842add8b340bd0eac692dfdde339ef6cd41840
austin9794 <mohdumair72914@gmail.com>
2025-12-04 23:57:28 +0000

    edit function

--- src/Controllers/AccountController.php
+++ src/Controllers/AccountController.php
@@ -131,4 +131,18 @@ class AccountController {
         header("Location: /Team-Project-Group-4/public/index.php?page=account&pw=success");
         exit;
     }
+
+    // Edit Account 
+    public function editAccountForm() {
+    requireLogin();
+
+    $db = Database::getInstance()->getConnection();
+
+    $stmt = $db->prepare("SELECT name, email, phone, address FROM users WHERE user_id = ?");
+    $stmt->execute([$_SESSION['user_id']]);
+    $user = $stmt->fetch();
+
+    include __DIR__ . '/../../templates/customer/account_edit.php';
+}
+
 }

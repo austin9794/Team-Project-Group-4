ac6871b8e6dc5e9f3a27f1199912502417f06aa5
Ikram Ali <IkramAli127@hotmail.com>
2025-12-04 10:10:32 +0000

    Add simple checkout form to place orders

--- templates/customer/checkout.php
+++ templates/customer/checkout.php
@@ -5,7 +5,8 @@
 <h2>checkout</h2>
 
 <form  method="post" action="/order/place">
-    
+    <p>This will place your order and clear your basket.</p>
+    <button type="submit">Place order</button>  
 </form>
 
 <?php include __DIR__ . '/../footer.php'; ?>

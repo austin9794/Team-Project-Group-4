55176d25fcca9e3e3939dd61690f6a819c8338c4
austin9794 <mohdumair72914@gmail.com>
2025-12-04 19:04:28 +0000

    edit

--- templates/auth/signup.php
+++ templates/auth/signup.php
@@ -1,7 +1,7 @@
 <?php include __DIR__ . '/../header.php'; ?>
 
 <style>
-/* Reuse login style */
+/* Reuse login style*/
 <?php echo file_get_contents(__DIR__ . '/../auth/login.css'); ?>
 </style>
 

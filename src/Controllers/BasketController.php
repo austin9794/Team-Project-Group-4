f9399e3b56a96fddc73062844aec7fc42d2b4a8c
austin9794 <mohdumair72914@gmail.com>
2025-12-04 00:03:48 +0000

    show basket

--- src/Controllers/BasketController.php
+++ src/Controllers/BasketController.php
@@ -9,27 +9,32 @@ class BasketController
     public function __construct() {
         $this->db = Database::getInstance()->getConnection();
     }
+
+    // SHOW BASKET PAGE
     
     public function index()
     {
-        session_start();
-        
-        $items = [] ;
+        $items = [];
         $total = 0;
 
-        //if basket isnt empty, build the items array
         if (!empty($_SESSION['basket'])) {
-            $productModel = new Product();
-            
-            foreach ($_SESSION['basket'] as $id => $qty) {
-                
-                $product = $productModel->find($id);
+
+            foreach ($_SESSION['basket'] as $productId => $qty) {
+
+                // Fetch product from DB
+                $stmt = $this->db->prepare("
+                    SELECT product_id, name, price
+                    FROM products 
+                    WHERE product_id = ?
+                ");
+                $stmt->execute([$productId]);
+                $product = $stmt->fetch();
 
                 if ($product) {
                     $lineTotal = $product['price'] * $qty;
 
                     $items[] = [
-                        'id'       => $id,
+                        'id'       => $product['product_id'],
                         'name'     => $product['name'],
                         'price'    => $product['price'],
                         'quantity' => $qty,
@@ -40,76 +45,3 @@ class BasketController
                 }
             }
         }
-
-        //load basket page
-        require __DIR__ . '/../../templates/customer/basket.php';
-    }
-
-    // add item to basket from product page
-    public function add()
-    {
-        session_start();
-        
-        $productId = $_POST['product_id'] ?? null;
-
-
-        //if for some reason no id came through just go back
-        if ($productId === null) {
-            header('Location: /products');
-            exit;
-        }
-        
-        if (!isset($_SESSION['basket'])) {
-            $_SESSION['basket'] = [];
-        }
-
-        if (isset($_SESSION['basket'][$productId])) {
-            $_SESSION['basket'][$productId]++;
-        } else {
-            $_SESSION['basket'][$productId] = 1;
-        }
-
-        header('Location: /products');
-        exit;
-    }
-
-    //update quantity from basket page (or remove if 0 or less)
-    public function update()
-    {
-        session_start();
-        
-        $productId = $_POST['product_id'] ?? null;
-        $quantity  = isset($_POST['quantity']) ? (int)$_POST['quantity'] : 0;
-
-        if ($productId === null) {
-            header('Location: /basket');
-            exit;
-        }
-
-        if ($quantity <= 0) {
-            unset($_SESSION['basket'][$productId]);
-        } else {
-            $_SESSION['basket'][$productId] = $quantity;
-        }
-
-        header('Location: /basket');
-        exit;
-    }
-
-    //completely remove item from basket
-    public function remove()
-    {
-        session_start();
-        
-        $productId = $_POST['product_id'] ?? null;
-
-        if ($productId !== null && isset($_SESSION['basket'][$productId])) {
-            unset($_SESSION['basket'][$productId]);
-        }
-        
-        header('Location: /basket');
-        exit;
-    }
-}
-
-?>

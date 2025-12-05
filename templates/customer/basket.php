82793f399e9fb39b11b19b10eba5650b305a109d
austin9794 <mohdumair72914@gmail.com>
2025-12-05 05:01:33 +0000

    modify buttons

--- templates/customer/basket.php
+++ templates/customer/basket.php
@@ -151,17 +151,17 @@
                         <form method="POST" action="/Team-Project-Group-4/public/index.php?page=basket-update">
                             <input type="hidden" name="product_id" value="<?= $item['id'] ?>">
                             <input type="hidden" name="quantity" value="<?= $item['quantity'] - 1 ?>">
-                            <button class="qty-btn">−</button>
+                            <button class="qty-btn minus" data-id="<?= $item['id'] ?>">−</button>
                         </form>
 
                         <!-- Quantity Display -->
-                        <div class="quantity-display"><?= $item['quantity'] ?></div>
+                        <div class="quantity-display" id="qty-<?= $item['id'] ?>"><?= $item['quantity'] ?></div>
 
                         <!-- Plus -->
                         <form method="POST" action="/Team-Project-Group-4/public/index.php?page=basket-update">
                             <input type="hidden" name="product_id" value="<?= $item['id'] ?>">
                             <input type="hidden" name="quantity" value="<?= $item['quantity'] + 1 ?>">
-                            <button class="qty-btn">+</button>
+                            <button class="qty-btn plus" data-id="<?= $item['id'] ?>">+</button>
                         </form>
 
                     </div>

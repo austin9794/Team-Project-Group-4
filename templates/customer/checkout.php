630292b51b57caa4c3c9f7ceb2f87e72125bb833
austin9794 <mohdumair72914@gmail.com>
2025-12-05 08:15:55 +0000

    load user details

--- templates/customer/checkout.php
+++ templates/customer/checkout.php
@@ -1,4 +1,14 @@
-<?php include __DIR__ . '/../header.php'; ?>
+<?php 
+require_once __DIR__ . '/../../src/Controllers/AccountController.php';
+
+$accountController = new AccountController();
+$userData = $accountController->getUserData(); 
+$addresses = $accountController->getAddresses();
+$paymentMethods = $accountController->getPaymentMethods();
+
+include __DIR__ . '/../header.php'; 
+
+?>
 
 <style>
 .checkout-container {

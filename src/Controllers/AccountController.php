aca0fde2f66535cf730d10ad7c94a9793c817246
austin9794 <mohdumair72914@gmail.com>
2025-12-05 03:34:34 +0000

    delete form

--- src/Controllers/AccountController.php
+++ src/Controllers/AccountController.php
@@ -212,6 +212,22 @@ public function updateAddress() {
     exit;
 }
 
+// Delete Address
+public function deleteAddress() {
+    requireLogin();
+
+    $id = $_GET['id'] ?? null;
+
+    if (!$id) exit("Invalid address");
+
+    $db = Database::getInstance()->getConnection();
+
+    $stmt = $db->prepare("DELETE FROM addresses WHERE address_id = ? AND user_id = ?");
+    $stmt->execute([$id, $_SESSION['user_id']]);
+
+    header("Location: /Team-Project-Group-4/public/index.php?page=account#addresses");
+    exit;
+}
 
     }
 

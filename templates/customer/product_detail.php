189446164aa15dec0eee600d7c98991d5df28c19
austin9794 <mohdumair72914@gmail.com>
2026-01-26 18:05:49 +0000

    full image clickable

--- templates/customer/product_detail.php
+++ templates/customer/product_detail.php
@@ -433,7 +433,18 @@ include __DIR__ . '/../header.php';
   <img id="modalImage" src="" alt="Product image fullscreen">
 
   <button class="modal-nav next">&#10095;</button>
-</div>
+  </div>
+
+  <div class="main-image zoom-container">
+  <img
+    id="mainProductImage"
+    class="zoom-image clickable"
+    src="<?= BASE_URL ?>assets/images/<?= htmlspecialchars($mainImage) ?>"
+    alt="<?= htmlspecialchars($product['name']) ?>"
+  >
+  <span class="view-full">View full image</span>
+  </div>
+
 
 </div>
 

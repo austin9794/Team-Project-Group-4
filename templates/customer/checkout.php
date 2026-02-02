63b1f0e3e86efcb1ccf8642b790dc4e773ca7473
austin9794 <mohdumair72914@gmail.com>
2026-02-02 21:14:09 +0000

    yeet default payment

--- templates/customer/checkout.php
+++ templates/customer/checkout.php
@@ -220,25 +220,6 @@ textarea {
   + Add a new payment method
 </a>
 
-    <!-- AUTO SELECT DEFAULT PAYMENT METHOD -->
-     <?php foreach ($payments as $p): ?>
-  <label class="payment-option">
-    <input
-      type="radio"
-      name="payment_id"
-      value="<?= $p['payment_id'] ?>"
-      <?= $p['is_default'] ? 'checked' : '' ?>
-      required
-    >
-    <?= htmlspecialchars($p['card_brand']) ?> ending <?= $p['card_last4'] ?>
-
-    <?php if ($p['is_default']): ?>
-      <span class="badge-default">Default</span>
-    <?php endif; ?>
-  </label>
-<?php endforeach; ?>
-
-
         <!-- ORDER SUMMARY -->
        <h2 class="section-title">Order Summary</h2>
 

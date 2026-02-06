916729fa0d6d45a1088e2ed32b5ab527886762c3
austin9794 <mohdumair72914@gmail.com>
2026-02-06 21:38:49 +0000

    fetch addresses

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -152,6 +152,14 @@ class OrderController {
     $userStmt->execute([$_SESSION['user_id']]);
     $userData = $userStmt->fetch();
 
+    $addrStmt = $db->prepare(" SELECT *
+        FROM addresses
+        WHERE user_id = ?
+        ORDER BY is_default DESC, created_at DESC
+    ");
+    $addrStmt->execute([$_SESSION['user_id']]);
+    $addresses = $addrStmt->fetchAll();
+
     
 }
 

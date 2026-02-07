406d2553964b4d6e8f621ce8317327a5b9a0b39f
austin9794 <mohdumair72914@gmail.com>
2026-02-07 21:15:41 +0000

    return form

---
+++ templates/customer/request_return.php
@@ -0,0 +1,15 @@
+<form method="POST" action="<?= BASE_URL ?>index.php?page=submit-return">
+    <input type="hidden" name="order_item_id" value="<?= $item['order_item_id'] ?>">
+
+    <label>Quantity to return</label>
+    <input type="number"
+           name="quantity"
+           min="1"
+           max="<?= $item['quantity'] - $item['returned_quantity'] ?>"
+           required>
+
+    <label>Reason</label>
+    <textarea name="reason" required></textarea>
+
+    <button class="btn-purple">Submit Return</button>
+</form>

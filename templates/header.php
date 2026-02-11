12816927325adb1d77cfe122677a5f1024d7de45
palakkaur4 <pkaurr95@gmail.com>
2026-02-11 00:42:11 +0000

    added a count number in the basket for customers to view how many items are being added

--- templates/header.php
+++ templates/header.php
@@ -265,7 +265,14 @@ require_once __DIR__ . '/../src/Config.php';
                 <circle cx="20" cy="21" r="1"></circle>
                 <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
             </svg>
-            <span>Basket</span>
+            <?php 
+            $basketCount = getBasketItemCount();
+            if ($basketCount > 0): 
+            ?>
+                <span>Basket (<?= $basketCount ?>)</span>
+            <?php else: ?>
+                <span>Basket</span>
+            <?php endif; ?>
         </a>
     </div>
 

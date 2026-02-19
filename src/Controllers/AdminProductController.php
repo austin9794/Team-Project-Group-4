6382aa5a03f78b5e815b3f11b6dd0618ce94663b
austin9794 <mohdumair72914@gmail.com>
2026-02-19 14:03:09 +0000

    add controller class

---
+++ src/Controllers/AdminProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/BaseAdminController.php';
+require_once __DIR__ . '/../Database.php';
+
+class AdminProductController extends BaseAdminController {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    
+
+    

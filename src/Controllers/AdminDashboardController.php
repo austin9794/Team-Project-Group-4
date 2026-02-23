d716f6efd1b6c11ceb3da897effd59ab40c7f901
austin9794 <mohdumair72914@gmail.com>
2026-02-23 15:11:48 +0000

    remove order controller

--- src/Controllers/AdminDashboardController.php
+++ src/Controllers/AdminDashboardController.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . '/BaseAdminController.php';
 require_once __DIR__ . '/../Database.php';
-require_once __DIR__ . '/OrderController.php';
 
 class AdminDashboardController extends BaseAdminController {
 

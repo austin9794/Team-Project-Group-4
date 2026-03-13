262d66cb21dca8b2adfc618d1e5f3fa8fc577d77
austin9794 <mohdumair72914@gmail.com>
2026-03-13 14:52:14 +0000

    Luhn Validation Function

--- templates/customer/add-payment.php
+++ templates/customer/add-payment.php
@@ -340,6 +340,36 @@ return true;
 
 }
 
+function luhnCheck(card){
+
+let sum = 0;
+let shouldDouble = false;
+
+card = card.replace(/\s/g,'');
+
+for(let i = card.length - 1; i >= 0; i--){
+
+let digit = parseInt(card.charAt(i));
+
+if(shouldDouble){
+
+digit *= 2;
+
+if(digit > 9){
+digit -= 9;
+}
+
+}
+
+sum += digit;
+shouldDouble = !shouldDouble;
+
+}
+
+return sum % 10 === 0;
+
+}
+
 });
 
 </script>

5ea12f5ca30e7c08ea593ce112d2a833b12f2f7d
austin9794 <mohdumair72914@gmail.com>
2026-03-15 15:13:05 +0000

    reset page

--- templates/auth/reset_password.php
+++ templates/auth/reset_password.php
@@ -1,1 +1,23 @@
-<!-- Password reset page -->
+<?php include __DIR__ . '/../header.php'; ?>
+
+<div class="auth-box">
+
+<h2>Create New Password</h2>
+
+<form method="POST" action="index.php?page=reset-password-submit">
+
+<input type="hidden" name="token" value="<?= htmlspecialchars($_GET['token']) ?>">
+
+<label>New Password</label>
+<input type="password" name="password" required>
+
+<label>Confirm Password</label>
+<input type="password" name="confirm" required>
+
+<button type="submit">Update Password</button>
+
+</form>
+
+</div>
+
+<?php include __DIR__ . '/../footer.php'; ?>
